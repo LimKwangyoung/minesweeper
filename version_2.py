16606204a2c4e681a3edef6ecffca422d1f34db8
kokoz22 <kokoz22@naver.com>
2023-06-18 00:37:34 +0900

    FEAT: add a click event

--- version_2.py
+++ version_2.py
@@ -15,30 +15,13 @@ class MineSweeper:
 
         # create initial boards of size (width * height).
         self.board = [[0] * self.width for _ in range(self.height)]
-        self.game_board = [['.'] * self.width for _ in range(self.height)]
+        self.game_board = [[''] * self.width for _ in range(self.height)]
 
         # variable for win.
         self.no_mine = 0
 
-        # GUI
-        self.root = tkinter.Tk()
-        self.root.title('지뢰 찾기')
-        self.root.geometry(f'{ROOT_WIDTH}x{ROOT_HEIGHT}')
-        self.root.resizable(False, False)
-
-        self.main_frame = tkinter.Frame(self.root, width=ROOT_WIDTH, height=ROOT_HEIGHT)
-        self.main_frame.pack()
-
-        for row in range(self.height):
-            for col in range(self.width):
-                btn = tkinter.Button(self.main_frame, padx=2, pady=2)
-                btn.bind('<Button-1>', lambda event, r=row, c=col: self.find_mine(event, r, c))
-                btn.bind('<Button-3>', )
-                btn.grid(row=row, column=col)
-
-
-        self.root.mainloop()
-
+        # buttons
+        self.buttons = [[None] * self.width for _ in range(self.height)]
 
     def set_mines(self) -> None:
         # set mines randomly.
@@ -57,76 +40,73 @@ class MineSweeper:
                     if (0 <= row <= self.height - 1) and (0 <= col <= self.width - 1) and self.board[row][col] != '*':
                         self.board[row][col] += 1
 
-    def graphic(self) -> tuple:
-        print('!!! 지뢰찾기 게임 !!!\n')
-        print('   |  ' + '  '.join(str(i % 10) for i in range(1, self.width + 1)))
-        print('------' + '--'.join('-' for _ in range(1, self.width + 1)))
-
-        for row in range(self.height):
-            print(f'{(row + 1) % 10}  |  ' + '  '.join(str(self.game_board[row][col]) for col in range(self.width)))
-        print()
-        print(f'깃발: {self.flag}')
-        print()
-        coord = input('원하는 타일의 행과 열을 입력하시오. (깃발은 F): ').split()
-
-        if len(coord) == 2:
-            row, col = coord
-            on_flag = None
-        elif len(coord) == 3:
-            row, col, on_flag = coord
-
-        return int(row) - 1, int(col) - 1, on_flag
-
-    def find_mine(self, row: int, col: int, on_flag) -> None:
+    def find_mine(self, row: int, col: int, on_flag=False) -> None:
         def expand_board(r: int, c: int) -> None:
             if self.board[r][c] != 0:
-                self.game_board[r][c] = self.board[r][c]
+                self.buttons[r][c]['text'] = self.game_board[r][c] = self.board[r][c]
                 self.no_mine += 1
                 return
 
             # the number around mines is 0.
-            self.game_board[r][c] = ' '
+            self.buttons[r][c]['text'] = self.game_board[r][c] = '0'
             self.no_mine += 1
 
             directions = ((r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
                           (r, c - 1), (r, c + 1),
                           (r + 1, c - 1), (r + 1, c), (r + 1, c + 1))
             for r, c in directions:
-                if (0 <= r <= self.height - 1) and (0 <= c <= self.width - 1) and self.game_board[r][c] == '.':
+                if (0 <= r <= self.height - 1) and (0 <= c <= self.width - 1) and self.game_board[r][c] == '':
                     expand_board(r, c)
+
+        # win a game.
+        if self.no_mine == self.width * self.height - self.mine:
+            print('!!! WIN !!!')
+            sys.exit()
 
         # plant a flag.
         if on_flag:
-            if on_flag in ('F', 'f') and self.flag > 0 and self.game_board[row][col] == '.':
+            if self.flag > 0 and self.game_board[row][col] == '':
                 self.game_board[row][col] = 'P'
                 self.flag -= 1
             else:
                 return
         # cancel a flag.
         elif self.game_board[row][col] == 'P':
-            self.game_board[row][col] = '.'
+            self.game_board[row][col] = ''
             self.flag += 1
-        # select a mine
+        # select a mine.
         elif self.board[row][col] == '*':
             print('!!! GAME OVER !!!')
             sys.exit()
         # not select a mine.
-        elif self.game_board[row][col] == '.':
+        elif self.game_board[row][col] == '':
             expand_board(row, col)
 
     def play(self):
         self.set_mines()
-        while True:
-            if self.no_mine == self.width * self.height - self.mine:
-                print('!!! WIN !!!')
-                sys.exit()
-            row, col, flag = self.graphic()
-            self.find_mine(row, col, flag)
 
-    def
+        # GUI
+        root = tkinter.Tk()
+        root.title('지뢰 찾기')
+        root.geometry(f'{ROOT_WIDTH}x{ROOT_HEIGHT}')
+        root.resizable(False, False)
+
+        main_frame = tkinter.Frame(root, width=ROOT_WIDTH, height=ROOT_HEIGHT)
+        main_frame.pack()
+
+        for row in range(self.height):
+            for col in range(self.width):
+                btn = tkinter.Button(main_frame, text=f'{self.game_board[row][col]}', padx=2, pady=2)
+                btn.bind('<Button-1>', lambda event, r=row, c=col: self.find_mine(r, c))
+                btn.bind('<Button-3>', lambda event, r=row, c=col: self.find_mine(r, c, on_flag=True))
+                btn.grid(row=row, column=col)
+
+                self.buttons[row][col] = btn
+
+        root.mainloop()
 
 
 if __name__ == '__main__':
-    print('! 지뢰찾기 게임 !\n')
-    game = MineSweeper(input('난이도를 입력하시오. (초급, 중급, 고급): '))
+    # game = MineSweeper(input('난이도를 입력하시오. (초급, 중급, 고급): '))
+    game = MineSweeper('초급')
     game.play()
